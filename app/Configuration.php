8b289e04e62725d1d46a89bf91c12238f3d1dbac
Prajakta <prajusisale777@gmail.com>
2018-02-01 13:50:36 +0530

    removed uppercase logic

--- app/Configuration.php
+++ app/Configuration.php
@@ -31,35 +31,6 @@ class Configuration extends Model
      */
     protected $fillable = ['conf_key', 'conf_value','status'];
 
-
-    /**
-     * Always capitalize the Configuration key when we retrieve it
-     */
-    public function getConfKeyAttribute($value) {
-        return ucfirst($value);
-    }
-
-    /**
-     * Always capitalize the configuration value when we retrieve it
-     */
-    public function getConfValueAttribute($value) {
-        return ucfirst($value);
-    }
-
-    /**
-     * Always capitalize the Configuration key when we save it to the database
-     */
-    public function setConfKeyAttribute($value) {
-        $this->attributes['conf_key'] = ucfirst($value);
-    }
-
-    /**
-     * Always capitalize the configuration value when we save it to the database
-     */
-    public function setConfValueAttribute($value) {
-        $this->attributes['conf_value'] = ucfirst($value);
-    }
-
-
+    
 
 }
