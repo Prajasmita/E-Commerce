5a3ec9de1160326a947facc6d0f67c33de31353f
Prajakta <prajusisale777@gmail.com>
2018-02-08 13:23:40 +0530

    deleted product_url and where it was used created that url with base_url

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,4 @@ return [
     'product_path' => '/img/product/',
     'shipping_Tax' => '50',
     'base_url' => 'http://127.0.0.1:8000/',
-    'product_details_url' => 'http://127.0.0.1:8000/product_details/',
-    'admin_note_url' => 'http://127.0.0.1:8000/admin/admin_note/',
 ];
