4d982488c3ecf7f88da83588bcb04f496e329f8e
Prajakta <prajusisale777@gmail.com>
2018-02-08 13:25:38 +0530

    added heart shape

--- public/js/user_wishlist.js
+++ public/js/user_wishlist.js
@@ -1,34 +1,6 @@
 $(document).ready(function(){
     $('.wishlist').on('click',function() {
         addToWishlist($(this));
-       /* console.log("hiii");
-        exit;*/
-        /*$.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        var product_id = ($(this).attr("data-id"));
-       // console.log(product_id);
-        $.ajax({
-            type : "POST",
-            url : wishlistUrl,
-            dataType : 'json',
-            data: { id: product_id },
-            success: function (data) {
-              //  console.log(data);exit;
-               if(data == "true")
-               {
-                   var html = "";
-                   html = "<a class=\"link_text_color\"><i class=\" glyphicon glyphicon-ok\"  ></i>Added to Wishlist</a>";
-                   $('#product_id_' + product_id).html(html);
-
-               }
-
-            }
-
-        });*/
     });
 
     function addToWishlist(thisVar) {
@@ -39,7 +11,7 @@ $(document).ready(function(){
         });
 
         var product_id = (thisVar.attr("data-id"));
-        console.log(product_id);
+        //console.log(product_id);
         $.ajax({
             type : "POST",
             url : wishlistUrl,
@@ -50,7 +22,7 @@ $(document).ready(function(){
                 if(data == "true")
                 {
                     var html = "";
-                    html = "<a class=\"link_text_color added\"><i class=\" fa fa-heart \"></i></a>";
+                    html = "<a class=\"added\"><i class=\" fa fa-heart \"></i></a>";
                     $('#product_id_' + product_id).html(html);
 
                 }
