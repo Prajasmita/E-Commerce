e64a92db500db26c87eb4c40eb75d6daa1a3ffaa
Prajasmita <prajusisale777@gmail.com>
2018-03-15 17:22:07 +0530

    added created_at field and admin as user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -4,7 +4,7 @@
 use Illuminate\Database\Seeder;
 Use App\User;
 use Illuminate\Support\Facades\DB;
-
+use Carbon\Carbon;
 class UserTableSeeder extends Seeder
 {
     /**
@@ -22,11 +22,16 @@ class UserTableSeeder extends Seeder
 
         User::truncate();
         $usersData = [
+            ['first_name'=>'Admin', 'last_name'=>'Admin','email'=>'admin@gmail.com'  ,'password' => Hash::make('admin123'),'contact_no' => '9821258699',
+                'role_id'=>'2','created_at' => Carbon::now(),
+
+            ],
             ['first_name'=>'Prajakta', 'last_name'=>'Sisale','email'=>'prajakta.neosoft@gmail.com'  ,'password' => Hash::make('prajakta123'),'contact_no' => '9821258699',
-                'role_id'=>'2'
+                'role_id'=>'2','created_at' => Carbon::now(),
+
             ],
             ['first_name'=>'Praju', 'last_name'=>'Sisale','email'=>'prajakta.sisale@neosofttech.com'  ,'password' => Hash::make('praju123'),'contact_no' => '9821258677',
-                'role_id'=>'5'
+                'role_id'=>'5','created_at' => Carbon::now(),
             ]
 
         ];
